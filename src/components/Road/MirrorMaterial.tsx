582f79300a985037f83bda876a8c291308b8b049
nuoqiqi <319799989@qq.com>
2024-02-05 14:34:09 +0800

    first commit

--- src/components/Road/MirrorMaterial.tsx
+++ src/components/Road/MirrorMaterial.tsx
@@ -2,7 +2,14 @@ import * as THREE from "three";
 import { extend, Object3DNode } from "@react-three/fiber";
 import vertex from "@/shader/Drops/drops.vert.glsl";
 import fragment from "@/shader/Drops/drops.frag.glsl";
-import { shaderMaterial } from "@react-three/drei";
+import { RenderTexture, shaderMaterial } from "@react-three/drei";
+import { useTexture } from "@react-three/drei";
+const textureRepeat = 30 / 2 / 2;
+
+// const tex = useTexture("./texture/TCom_Asphalt_Broken_header.jpg", (t) => {
+//   t.wrapS = t.wrapT = THREE.ClampToEdgeWrapping;
+//   t.repeat.set(textureRepeat, textureRepeat);
+// });
 type MirrorMaterialType = JSX.IntrinsicElements["shaderMaterial"] & {
   uniforms?: {
     u_RainProgress: { value: number };
@@ -21,13 +28,10 @@ const MirrorMaterial = shaderMaterial(
     u_resolution: new THREE.Vector2(),
     u_mouse: new THREE.Vector2(),
     u_color: new THREE.Color(0x000000),
+    // texture: tex,
   },
   vertex,
-  fragment,
-  (material) => {
-    material.side = THREE.DoubleSide;
-    material.transparent = true;
-  }
+  fragment
 );
 extend({ MirrorMaterial });
 export default MirrorMaterial;
--- src/components/Road/index.tsx
+++ src/components/Road/index.tsx
@@ -9,17 +9,11 @@ extend({ MirrorMaterial });
 
 export default function Floor() {
   const size = 30;
-  const textureRepeat = 30 / 2 / 2;
-  const tex = useTexture("./texture/TCom_Asphalt_Broken_header.jpg", (t) => {
-    t.wrapS = t.wrapT = THREE.ClampToEdgeWrapping;
-    t.repeat.set(textureRepeat, textureRepeat);
-  });
 
   return (
     <mesh castShadow receiveShadow rotation-x={Math.PI / -2}>
       <circleGeometry args={[size, size]} />
-      <meshBasicMaterial map={tex}></meshBasicMaterial>
-      {/* <mirrorMaterial /> */}
+      <mirrorMaterial />
     </mesh>
   );
 }
